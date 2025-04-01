06bd6220cbb0b2b97a076e21788f05ae8fa4cede
devin_1 <22dcs001@charusat.edu.in>
2025-04-01 22:54:54 +0530

    API_URI Changes!

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-export const API_URI = "https://rbhardware-backend.onrender.com"
+// export const API_URI = "https://rbhardware-backend.onrender.com"
+// export const API_URI = "http://localhost:8000"
+export const API_URI = "https://api.rbhardware.in"
